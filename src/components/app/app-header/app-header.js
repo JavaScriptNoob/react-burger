7a270cd3e728fb622c8b82622f50cf643d66aab0
JavascripNoob <timur.bobylev@yandex.ru>
2023-02-06 23:46:39 +0100

    appbar

--- src/components/app/app-header/app-header.js
+++ src/components/app/app-header/app-header.js
@@ -15,12 +15,12 @@ class AppHeader extends React.Component {
     render() {
 
         return (
-            <div className="pl-5 pr-5 pb-5 pt-5">
-                <ul className="navbarList">
-                    <li className="pl-5 pr-5 pb-5 pt-5 "><BurgerIcon  type="primary"/> <a  href="" className="pl-1" > Конструктор</a></li>
-                    <li className="pl-5 pr-5 pb-5 pt-5"><ListIcon type="primary"/><a href="" className="pl-1">Лента Заказов</a></li>
+            <div className="pl-5 pr-5  ">
+                <ul className="navbarList m-1">
+                    <li className=" "><BurgerIcon  type="primary"/> <a  href="" className="pl-1" > Конструктор</a></li>
+                    <li className="pl-4"><ListIcon type="primary"/><a href="" className="pl-1">Лента Заказов</a></li>
                     <li style={{marginLeft: 112} } ><a href=""  ><Logo className="logo"/></a></li>
-                    <li className="" style={{marginLeft: 288} }><ProfileIcon type="primary" /><a href="" className="pl-1">Личный кабинет</a></li>
+                    <li className="pl-3" style={{marginLeft: 288} }><ProfileIcon type="primary" /><a href="" className="pl-1">Личный кабинет</a></li>
                 </ul>
 
             </div>

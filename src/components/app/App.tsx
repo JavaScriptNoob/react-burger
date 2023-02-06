975aa275b4cac3bdb524a91ac53f08fe5dc71d3e
JavascripNoob <timur.bobylev@yandex.ru>
2023-02-06 23:46:39 +0100

    appbar

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 
 import './App.css';
 import AppHeader from './app-header/app-header'
-import Data from "../utils/data";
+import data from "../utils/data";
 import BurgerIngredients from "./burger-ingredients/burger-ingredients";
+
 class  App extends React.Component {
 
-    state = {
-        Data
-}
+
 
     render (){
         console.log(this.state)
         return (
             <div className="App">
                 <AppHeader/>
-                <BurgerIngredients/>
+                <BurgerIngredients data ={data} />
             </div>
         );
     }
--- src/components/app/burger-ingredients/burger-ingredients.js
+++ src/components/app/burger-ingredients/burger-ingredients.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import {
-    Counter,
-    Logo,
-    BurgerIcon,
-    ListIcon,
-    MenuIcon,
-    ProfileIcon
+
+import {Tab,
+    ConstructorElement,
+    Counter, CurrencyIcon
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from './burger-ingrediens.module.css'
 
@@ -13,24 +10,93 @@ class BurgerIngredients extends React.Component {
 
 
     render() {
+        console.log(this.props.data)
+        // const  data =[...this.props]
+        // console.log(data)
 
         return (
-            <div className="pl-5 pr-5 pb-5 pt-5">
+            <div className={styles.containerGeneral}>
 
-                <div>
-                    <h1>Соберите бургер</h1>
-                    <div>
-                        <div>Булки</div>
-                        <div>Соусы</div>
-                        <div>Начинки</div>
+                <div className={styles.containerMain}>
+                    <div className={styles.containerSub}>
+                        <div className={styles.containerTitle}>
+                            <h1 className="text text_type_main-large">Соберите бургер</h1>
+                        </div>
+                        <div className={styles.containerSubTitle}>
+                            <div  className={styles.scrollContainer}> <div style={{ display: 'flex' }}>
+                                <Tab value="one"  >
+                                    One
+                                </Tab>
+                                <Tab value="two" >
+                                    Two
+                                </Tab>
+                                <Tab value="three" >
+                                    Three
+                                </Tab>
+                            </div> <h2>Булки</h2>
+                                <div style={{display:"flex", flexWrap:"wrap"}}>{this.props.data.filter(item => item.type ===('bun')).map(bunItem => (
+                                    <div>
+                                        <div>
+                                            <div className= {"pl-4 pr-4 pb-1 pt-6 " +styles.counterRelative}>
+                                                <img src={bunItem.image} alt=""/>
+                                                <Counter count={1} size="default" extraClass="m-1" />
+                                            </div>
+
+                                            <div className={styles.price}><p>
+                                                {bunItem.price}
+                                                <i className="pl-3"><CurrencyIcon className="pl-3" type='primary'/></i>
+                                            </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}</div>
+
+                                <h2>Соусы</h2>
+                                <div style={{display:"flex",flexWrap:"wrap"}}>{this.props.data.filter(item => item.type ===('sauce')).map(sauceItem => (
+                                    <div>
+                                        <div>
+                                            <div className= {"pl-4 pr-4 pb-1 pt-6 " +styles.counterRelative}>
+                                                <img src={sauceItem.image} alt=""/>
+                                                <Counter count={1} size="default" extraClass="m-1" />
+                                            </div>
+
+                                            <div className={styles.price}><p>
+                                                {sauceItem.price}
+                                                <i className="pl-3"><CurrencyIcon className="pl-3" type='primary'/></i>
+                                            </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}</div>
+                                <h2>Соусы</h2>
+                                <div style={{display:"flex",flexWrap:"wrap"}}>{this.props.data.filter(item => item.type ===('main')).map(sauceItem => (
+                                    <div>
+                                        <div>
+                                            <div className= {"pl-4 pr-4 pb-1 pt-6 " +styles.counterRelative}>
+                                                <img src={sauceItem.image} alt=""/>
+                                                <Counter count={1} size="default" extraClass="m-1" />
+                                            </div>
+
+                                            <div className={styles.price}><p>
+                                                {sauceItem.price}
+                                                <i className="pl-3"><CurrencyIcon className="pl-3" type='primary'/></i>
+                                            </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}</div>
+
+                            </div>
+
+                        </div>
                     </div>
                 </div>
                 <div>
                     <h1>Булки</h1>
                     <div>
-                        <div>Булки</div>
-                        <div>Соусы</div>
-                        <div>Начинки</div>
+                        <div style={{display: 'flex', flexDirection: 'column', gap: '10px'}}>
+
+                        </div>
                     </div>
                 </div>
                 <div>
